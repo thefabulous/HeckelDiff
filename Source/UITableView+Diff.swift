247a9a2c06a7203c66a44ea8a8f5c9a6527dbca3
Tomek Kopycki <tomasz@thefabulous.co>
2018-08-10 11:18:06 +0200

    Use reloadVisible flag to reload visible cells during updates

--- Source/UITableView+Diff.swift
+++ Source/UITableView+Diff.swift
@@ -18,28 +18,39 @@ public extension UITableView {
     /// - parameter insertAnimation: The animation type for insertion.
     /// - parameter deleteAnimation: The animation type for deletion.
     /// - parameter reloadAnimation: The animation type for reload.
-    func applyDiff<T: Collection>(_ old: T, _ new: T, inSection section: Int, withInsertAnimation insertAnimation: UITableViewRowAnimation, withDeleteAnimation deleteAnimation: UITableViewRowAnimation, withReloadAnimation reloadAnimation: UITableViewRowAnimation, completion: @escaping () -> Void) where T.Iterator.Element: Hashable, T.IndexDistance == Int, T.Index == Int {
-        let update = ListUpdate(diff(old, new), section)
+    func applyDiff<T: Collection>(_ old: T, _ new: T, inSection section: Int,
+                                  withInsertAnimation insertAnimation: UITableViewRowAnimation,
+                                  withDeleteAnimation deleteAnimation: UITableViewRowAnimation,
+                                  withReloadAnimation reloadAnimation: UITableViewRowAnimation,
+                                  reloadVisible: Bool = false,
+                                  completion: @escaping () -> Void)
+        where T.Iterator.Element: Hashable, T.IndexDistance == Int, T.Index == Int {
 
-        CATransaction.begin()
-        CATransaction.setCompletionBlock(completion)
-        
-        beginUpdates()
-        deleteRows(at: update.deletions, with: deleteAnimation)
-        insertRows(at: update.insertions, with: insertAnimation)
-        for move in update.moves {
-            moveRow(at: move.from, to: move.to)
-        }
-        endUpdates()
-        
-        // reloadItems is done separately as the update indexes returne by diff() are in respect to the
-        // "after" state, but the collectionView.reloadItems() call wants the "before" indexPaths.
-        if update.updates.count > 0 {
+            let update = ListUpdate(diff(old, new), section)
+
+            CATransaction.begin()
+            CATransaction.setCompletionBlock(completion)
+
             beginUpdates()
-            reloadRows(at: update.updates, with: reloadAnimation)
+            deleteRows(at: update.deletions, with: deleteAnimation)
+            insertRows(at: update.insertions, with: insertAnimation)
+            for move in update.moves {
+                moveRow(at: move.from, to: move.to)
+            }
             endUpdates()
-        }
-        
-        CATransaction.commit()
+
+            // reloadItems is done separately as the update indexes returned by diff() are in respect to the
+            // "after" state, but the collectionView.reloadItems() call wants the "before" indexPaths.
+            if !update.updates.isEmpty || reloadVisible {
+                var rows = update.updates
+                if reloadVisible {
+                    rows += (indexPathsForVisibleRows ?? [])
+                }
+                beginUpdates()
+                reloadRows(at: rows, with: reloadAnimation)
+                endUpdates()
+            }
+
+            CATransaction.commit()
     }
 }
